fe2e4d7462e2aa059cd1b2172e93cac0325cad2a
jennasaikaly <jen.saikaly@gmail.com>
2022-04-03 22:49:48 -0400

    added initial docs and html structure, started adding fetch api functions

---
+++ Assets/JS/script.js
@@ -0,0 +1,13 @@
+
+let cityName = document.getElementById('#');
+let stateCode = document.getElementById('#');
+let countryCode = document.getElementById('#');
+
+
+var getCityName = function (
+    
+)
+
+var getWeatherinfo = function {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + stateCode + "," countryCode + "&limit=5&appid=ffaf5b955f9d8df5264787907b066904"
+}
