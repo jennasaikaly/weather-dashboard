b701c4873147daf4476c20cbd3d9060c8d779534
jennasaikaly <jen.saikaly@gmail.com>
2022-07-04 16:07:27 -0400

    got 5 day forecast to display dynamically

--- Assets/JS/homepage.js
+++ Assets/JS/homepage.js
@@ -4,10 +4,14 @@
 
   var currentContainerEl = document.querySelector("#current-weather-container");
   var citySearchTerm = document.querySelector("#city-search-term");
+  var currentDateEl = document.querySelector("#current-date")
+  var currentIconEl = document.querySelector("#current-icon");
   var currentTempEl = document.querySelector("#current-temp");
   var currentWindEl = document.querySelector("#current-wind");
   var currentHumidityEl = document.querySelector("#current-humidity");
   var currentUvEl = document.querySelector("#current-uv");
+
+  var dailyWeatherContainerEl = document.querySelector("#daily-weather-container");
 
 
   var formSubmitHandler = function(event) {
@@ -46,33 +50,85 @@ var getWeather = function (lat,lon) {
         response.json().then(function (data) {
             console.log(data);
             var cityname = nameInputEl.value.trim();
-            displayWeather(data, cityname);
+            displayCurrentWeather(data, cityname);
+            displayForecast(data, cityname);
         });
     });
 };
 
-var displayWeather = function(data, cityname) {
-// console.log(data);
-    // clear old content
+var displayCurrentWeather = function(data, cityname) {
 
-citySearchTerm.textContent = cityname;
-const {temp, wind_speed, humidity, uvi} = data.current;
-
-
+// clear old content
+currentDateEl.textContent = "";
+currentIconEl.textContent = "";
 currentTempEl.textContent = "";
 currentWindEl.textContent = "";
 currentHumidityEl.textContent = "";
 currentUvEl.textContent = "";
 
-currentTempEl.textContent = "Temp: " +temp + "°C";
+console.log(data);
+
+
+
+const icon = data.current.weather[0].icon ;
+const {dt, temp, wind_speed, humidity, uvi} = data.current;
+
+citySearchTerm.textContent = cityname;
+currentDateEl.textContent = dt;
+currentIconEl.src = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+currentTempEl.textContent = "Temp: " +temp + " °C";
 currentWindEl.textContent = "Wind: " + wind_speed + " meters/sec";
 currentHumidityEl.textContent = "Humidity: " + humidity + "%";
-currentUvEl.textContent = "UV Index: " + uvi;
 
+currentUvEl.textContent = "UV Index: " + uvi;
+currentUvEl.style.backgroundColor = "red";
  
 
 
 };
 
+var displayForecast = function(data, cityname){
+    
+    // loop over forecasts
+for (var i = 0; i < 5; i++) {
+    // format repo name
+    var dailyDate = data.daily[i].dt;
+    var dailyIcon = data.daily[i].weather[0].icon
+    var dailyTemp = data.daily[i].temp.day;
+    var dailyWind = data.daily[i].wind_speed;
+    var dailyHumidity = data.daily[i].humidity;
+  
+    // create a container for each daily forecast
+    var dailyEl = document.createElement("div");
+    dailyEl.classList = "list-item flex-row justify-space-between align-center";
+  
+    // create a span element to hold each Date Title
+    var titleEl = document.createElement("span");
+    titleEl.textContent = dailyDate;
+
+    var dailyIconEl = document.createElement("img");
+    dailyIconEl.src = "http://openweathermap.org/img/wn/"+ dailyIcon +"@2x.png";
+
+    var dailyTempEl = document.createElement("span");
+    dailyTempEl.textContent = "Temp: " + dailyTemp + " °C";
+
+    var dailyWindEl = document.createElement("span");
+    dailyWindEl.textContent = "Wind: " +dailyWind + "meters/sec";
+
+    var dailyHumidityEl = document.createElement("span");
+    dailyHumidityEl.textContent = "Humidity: " + dailyHumidity + "%";
+  
+    // append to container
+    dailyEl.appendChild(titleEl);
+    dailyEl.appendChild(dailyIconEl);
+    dailyEl.appendChild(dailyTempEl);
+    dailyEl.appendChild(dailyWindEl);
+    dailyEl.appendChild(dailyHumidityEl);
+  
+    // append container to the dom
+    dailyWeatherContainerEl.appendChild(dailyEl);
+  }
+
+};
 
   cityFormEl.addEventListener("submit", formSubmitHandler);
